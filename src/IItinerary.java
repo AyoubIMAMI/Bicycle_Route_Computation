032c5d60900b00ff095bc8b118847b6dc9d0ac45
AyoubIMAMI <youbima17@gmail.com>
2022-11-16 17:55:44 +0100

    Javadoc added to getItinerary in IItinerary

--- src/IItinerary.java
+++ src/IItinerary.java
@@ -3,5 +3,11 @@ import java.rmi.RemoteException;
 
 public interface IItinerary extends Remote {
 
+    /**
+     * Get itinerary between an origin and a destination
+     * @param destinationAddress user destination address
+     * @param originAddress user origin address
+     * @throws RemoteException exception
+     */
     void getItinerary(String destinationAddress, String originAddress) throws RemoteException;
 }
